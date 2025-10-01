ce6177c57bd3f634d6df1cd9f9f5d557ddee7a67
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-10-01 20:13:46 +0200

    [FEATURE] Add more colors.

--- src/color.zig
+++ src/color.zig
@@ -1,18 +1,68 @@
 const std = @import("std");
 
-pub const Colors = struct {
-    pub const White   = [3]u8{ 255, 255, 255 };
-    pub const Black   = [3]u8{   0,   0,   0 };
-    pub const Red     = [3]u8{ 255,   0,   0 };
-    pub const Green   = [3]u8{   0, 255,   0 };
-    pub const Blue    = [3]u8{   0,   0, 255 };
-    pub const Yellow  = [3]u8{ 255, 255,   0 };
-    pub const Cyan    = [3]u8{   0, 255, 255 };
-    pub const Magenta = [3]u8{ 255,   0, 255 };
-    pub const Orange  = [3]u8{ 255, 128,   0 };
-    pub const Purple  = [3]u8{ 128,   0, 128 };
-    pub const Gray    = [3]u8{ 128, 128, 128 };
+pub const Color = enum {
+    White,
+    Black,
+    Red,
+    Green,
+    Blue,
+    Yellow,
+    Cyan,
+    Magenta,
+    Orange,
+    Purple,
+    Gray,
+    Pink,
+    Brown,
+    Gold,
+    Silver,
+    Lime,
+    Aqua,
+    Navy,
+    Teal,
+    NeonPink, 
+    NeonGreen, 
+    NeonBlue, 
+    NeonYellow,
+    NeonOrange, 
+    NeonPurple, 
+    NeonCyan, 
+    NeonRed,
 };
+
+pub const color_values = [_][3]u8{
+    .{255, 255, 255}, // White
+    .{0,   0,   0},   // Black
+    .{255, 0,   0},   // Red
+    .{0,   255, 0},   // Green
+    .{0,   0, 255},   // Blue
+    .{255, 255, 0},   // Yellow
+    .{0,   255, 255}, // Cyan
+    .{255, 0, 255},   // Magenta
+    .{255, 128, 0},   // Orange
+    .{128, 0, 128},   // Purple
+    .{128, 128, 128}, // Gray
+    .{255, 192, 203}, // Pink
+    .{165, 42,  42},  // Brown
+    .{255, 215, 0},   // Gold
+    .{192, 192, 192}, // Silver
+    .{50,  205, 50},  // Lime
+    .{127, 255, 212}, // Aqua
+    .{0,   0,   128}, // Navy
+    .{0,   128, 128}, // Teal
+    .{255,   0, 144}, // NeonPink
+    .{57,  255, 20},  // NeonGreen
+    .{0,   191, 255}, // NeonBlue
+    .{207, 255, 4},   // NeonYellow
+    .{255, 95,  31},  // NeonOrange
+    .{191, 0,   255}, // NeonPurple
+    .{0,   255, 255}, // NeonCyan
+    .{255, 16,  83},  // NeonRed
+};
+
+pub fn rgbOf(c: Color) [3]u8 {
+    return color_values[@intFromEnum(c)];
+}
 
 pub const ColorScale = struct {
     allocator: *std.mem.Allocator,
